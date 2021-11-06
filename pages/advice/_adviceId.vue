8c03cacdebe44a5c52c17fc7af3176503a3d5f92
Collins Zimba <dev@madebycollins.com>
2021-11-06 22:24:39 +0200

    Added page to show advice by ID

---
+++ pages/advice/_adviceId.vue
@@ -0,0 +1,28 @@
+<template>
+<h1> {{ advice.advice }} </h1>
+</template>
+
+<script>
+export default {
+  name: 'AdviceId',
+  data() {
+    return {
+      adviceId: this.$route.params.adviceId,
+      advice: {}
+    }
+  },
+  mounted() {
+    this.loadAdviceById()
+  },
+  methods: {
+    async loadAdviceById(){
+      // Get the advice
+      this.advice = await this.$store.dispatch('advice/getAdviceSlipById', this.adviceId)
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
