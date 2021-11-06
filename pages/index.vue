460c11b4ae30d57c955aff5da506a8d2618cdd55
Collins Zimba <dev@madebycollins.com>
2021-11-06 22:53:54 +0200

    Updated the index page to something more informative

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,22 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-    </div>
+    <h2 class="title is-3">Random advice app</h2>
+    <p>This application fetches random advice from the Random Advice API: <a href='https://api.adviceslip.com/advice'>https://api.adviceslip.com/advice</a></p>
+    <br/>
+    <p>Built with <a href="https://nuxtjs.org/">Nuxt.js</a>, <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a></p>
+    <br/>
+    <p>Open source on <a href="https://github.com/madebycollins/randomadviceapp">GitHub</a></p>
   </section>
 </template>
 
-<script>
-import Card from '~/components/Card'
+<style>
+h2.title{
+  color: black;
+}
+</style>
 
+<script>
 export default {
   name: 'HomePage',
-
-  components: {
-    Card
-  }
 }
 </script>
