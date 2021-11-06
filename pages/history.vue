88ebc46832457db2e43ed43a42b9132252cb6631
Collins Zimba <dev@madebycollins.com>
2021-11-06 22:40:52 +0200

    Added page to show advice history

---
+++ pages/history.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class='section'>
+    <h2 v-for='advice in adviceList' :key="advice.id" class='title is-3'>
+      "{{ advice.advice }}"
+    </h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'History',
+  data() {
+    return {
+      adviceList: {}
+    }
+  },
+  mounted() {
+    this.loadAdviceHistory()
+  },
+  methods: {
+    async loadAdviceHistory() {
+      // Get the advice
+      this.adviceList = await this.$store.getters['advice/getAdviceList']
+    }
+  }
+}
+</script>
